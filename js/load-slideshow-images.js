a2b5e75e5377f268d4b87a2526ebd6c04d25e30c
curtishowell <github@curtishowell.net>
2013-03-26 16:31:49 -0700

    need to look at last image to know the images have been loaded

--- js/load-slideshow-images.js
+++ js/load-slideshow-images.js
@@ -37,8 +37,7 @@ $(function() { //dom ready
 
 
 			//when the last image has loaded (decent indication that the slideshow is ready)...
-			//$('#main_content_slideshow').children().last().load(function(){
-			$('#main_content_slideshow').load(function(){
+			$('#main_content_slideshow').children().last().load(function(){
 
 				//start the slideshow...
 				$('#main_content_slideshow').cycle({
