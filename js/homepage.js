09355b1ba24e62d0996e4caef9a55be72c9d452c
Curtis Howell <github@curtishowell.net>
2013-05-01 08:56:15 -0700

    new home page video

--- js/homepage.js
+++ js/homepage.js
@@ -16,5 +16,7 @@ function removeSlideshow(){
 
 function addVideo(){
 	var videoEmbed = '<iframe src="http://fast.wistia.net/embed/iframe/pqyhr9z28y?autoPlay=true&controlsVisibleOnLoad=true&version=v1&videoHeight=540&videoWidth=960&volumeControl=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" width="960" height="540"></iframe>'
+	$('#main_content_home').css('height', '540px')
+
 	$('#main_content_home').prepend(videoEmbed)
 }
